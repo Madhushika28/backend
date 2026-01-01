9329464408a5200c52615106a82872164fa3d38c
Madhushika <madhushikaireshamali@gmail.com>
2026-01-01 22:17:54 +0530

    update index.js

--- index.js
+++ index.js
@@ -115,11 +115,9 @@ app.use("/api/payments", paymentRouter);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, 
-    () => {
-        console.log("Server is running on port 5000");
-    }
-)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 
 
